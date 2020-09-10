ed3a65c1ca9d73749bc5917b1155991d44f9e35f
James Belton <james.belton@uk.ibm.com>
2020-09-10 09:54:10 +0100

    Update run_commands.sh

--- vpc-scripts/run_commands.sh
+++ vpc-scripts/run_commands.sh
@@ -2,7 +2,7 @@
 runcmd:
  - add-apt-repository main
  - apt-get install s3fs
- - echo 75225bcb880148eea9cf5b4dcf9ae5a3:7c3ce36ae0c4631440051f79ecf98b7da2eda348586726d6 > /root/.passwd-s3fs
+ - echo <<HMAC secret>> > /root/.passwd-s3fs
  - chmod 600 /root/.passwd-s3fs
  - mkdir /mycosbucket
  - s3fs rules-public /mycosbucket -o passwd_file=/root/.passwd-s3fs -o url=https://s3.eu-gb.cloud-object-storage.appdomain.cloud
